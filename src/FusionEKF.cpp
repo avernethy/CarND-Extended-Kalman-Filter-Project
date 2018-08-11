3b910e3d4cceeaf920dcfa828327f81a84b553e5
Vern Francisco <avernethy@gmail.com>
2018-08-10 23:09:02 -0500

    try using x_ assignment from laser measurements part 4

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -70,7 +70,8 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
     cout << "EKF: " << endl;
     ekf_.x_ = VectorXd(4);
     //ekf_.x_ << 1, 1, 1, 1;
-    ekf_.x_ << measurement_pack.raw_measurements_;
+    //ekf_.x_ << measurement_pack.raw_measurements_; This doesn't work
+    ekf_.x_ << measurement_pack.raw_measurements_[0], measurement_pack.raw_measurements_[1], 0, 0;
     cout << "reading measurement " << endl;
     cout << ekf_.x_ << endl;
 
