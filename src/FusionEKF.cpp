6c59099ebcc7a4946eac797a0e3353d56409e517
Vern Francisco <avernethy@gmail.com>
2018-08-11 00:02:57 -0500

    Remove assignement of dt for now

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -126,7 +126,7 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
      * Update the process noise covariance matrix.
      * Use noise_ax = 9 and noise_ay = 9 for your Q matrix.
    */
-  ekf_.F_(0,2) << dt;
+  //ekf_.F_(0,2) << dt;
   ekf_.Predict();
 
   /*****************************************************************************
