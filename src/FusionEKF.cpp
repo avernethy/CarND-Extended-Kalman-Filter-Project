6cbd509fd70f4cd2b7b9367e635f36320438b7a0
Vern Francisco <avernethy@gmail.com>
2018-08-11 19:07:00 -0500

    Fix Calc Jacobian call

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -164,7 +164,7 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
   if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
     // Radar updates
     ekf_.R_ = R_radar_;
-    ekf_.H_ = Tools::CalculateJacobian(const VectorXd& ekf_.x_);
+    ekf_.H_ = tools.CalculateJacobian(ekf_.x_);
     ekf_.UpdateEKF(measurement_pack.raw_measurements_);
     
   } else {
