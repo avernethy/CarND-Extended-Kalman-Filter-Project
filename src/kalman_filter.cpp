ae432f943777a700abf0cc2c9b0d961840058a51
Vern Francisco <avernethy@gmail.com>
2018-08-11 22:16:10 -0500

    fix atan call

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -101,7 +101,7 @@ void KalmanFilter::UpdateEKF(const VectorXd &z) {
  float px = x_[2];
  float py = x_[3];
  z_pred[0] = sqrt(px*px + py * py);
- z_pred[1] = atan2(py/px);
+ z_pred[1] = atan2(py, px);
  z_pred[2] = (py*vy + px*vx) / z_pred[0];
  cout << "phi = " << z_pred[1] <<endl;
  
